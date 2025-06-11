a6012f9381b999e629f0e62af4bb9f1a99cbbc68
testtsts <ihsannur0610@gmail.com>
2025-06-11 20:04:17 +0700

    demoblaze

---
+++ tests/demoblaze/order.spec.js
@@ -0,0 +1,56 @@
+const {fakerID_ID: faker} = require('@faker-js/faker');
+const {HOMEPAGE_URL,DemoBlazePom} = require("./pom");
+const { test, expect} =  require('@playwright/test');
+
+
+test.describe("Demo Blaze Order", function(){
+
+    test.beforeEach(async ({page})=>{
+        const demoBlaze = new DemoBlazePom(page);
+        await demoBlaze.visit();
+        expect(page.url()).toEqual(HOMEPAGE_URL);
+        // cy.url().should("eq",HOMEPAGE_URL)
+    })
+    
+    test.only("should success order item from cart", async({page})=>{
+        const demoBlaze = new DemoBlazePom(page)
+        await demoBlaze.selectItemByName("Samsung galaxy s6");
+        await demoBlaze.addToCart();
+        // cy.go(-2);
+        await page.goBack(-2)
+        await demoBlaze.selectItemByName("Nokia lumia 1520");
+        await demoBlaze.addToCart();
+        await demoBlaze.goToCart();
+        await demoBlaze.getTotalPrice().should('have.text', "1180");
+        await demoBlaze.placeOrder();
+        await demoBlaze.fillName(faker.person.fullName())
+        await demoBlaze.fillCountry("Indonesia");
+        await demoBlaze.fillCity(faker.location.city());
+        await demoBlaze.fillCard(faker.finance.accountNumber());
+        await demoBlaze.fillMonth(faker.date.month());
+        await demoBlaze.fillYear("2030");
+        await demoBlaze.clickPurchase();
+        await demoBlaze.closeOrderSummary();
+        cy.reload()
+        await demoBlaze.getTotalPrice().should('not.be.visible');
+    })
+
+    test("should success delete item from cart", async ({page})=>{
+        DemoBlazePom.selectItemByName("Samsung galaxy s6");
+        DemoBlazePom.addToCart();
+        cy.go(-2);
+        DemoBlazePom.selectItemByName("Sony xperia z5");
+        DemoBlazePom.addToCart();
+        cy.go(-2);
+        DemoBlazePom.selectItemByName("HTC One M9");
+        DemoBlazePom.addToCart();
+        cy.go(-2);
+        DemoBlazePom.selectItemByName("Nexus 6");
+        DemoBlazePom.addToCart();
+        DemoBlazePom.goToCart();
+        DemoBlazePom.getTotalPrice().should('have.text', "2030")
+        // DemoBlazePom.deleteTopItem();
+        DemoBlazePom.removeItemFromCartByName("HTC One M9")
+        DemoBlazePom.getTotalPrice().should('have.text', "1330")
+    })
+})
---
+++ tests/demoblaze/pom.js
@@ -0,0 +1,74 @@
+const { expect } = require("playwright/test");
+
+exports.DemoBlazePom = class DemoBlazePom{
+
+    /**
+     * @param {import('@playwright/test').Page} page
+     */
+    constructor(page){
+        this.page= page
+    }
+
+    async visit(){
+        return this.page.goto('https://www.demoblaze.com/')
+    }
+
+    async selectItemByName(itemName){
+        return this.page.locator(".card-title").filter({hasText:itemName}).click();
+    }
+
+    async addToCart(){
+        return this.page.locator(".btn.btn-success.btn-lg").click();
+    }
+
+    async goToCart(){
+        return this.page.locator("#cartur").click()
+    }
+
+    async removeItemFromCartByName(itemName){
+        return this.page.locator(".success").filter({hasText:itemName}).locator("..").filter({hasText:"Delete"}).click()
+    }
+
+    async getTotalPrice(){
+        return this.page.locator("#totalp")
+    }
+
+    async placeOrder(){
+        return this.page.locator(".btn.btn-success").click();
+    }
+
+    async fillName(name){
+        return this.page.locator("#name").fill(name);
+    }
+    
+    async fillCountry(country){
+        return this.page.locator("#country").fill(country);
+    }
+
+    async fillCity(city){
+        return this.page.locator('#city').fill(city);
+    }
+
+    async fillCard(card){
+        return this.page.locator('#card').fill(card)
+    }
+
+    async fillMonth(month){
+        return this.page.locator('#month').fill(month)
+    }
+
+    async fillYear(year){
+        return this.page.locator('#year').fill(year)
+    }
+
+    async clickPurchase(){
+        return this.page.locator(".btn.btn-primary").filter({hasText:"Purchase"}).click();
+    }
+
+    async closeOrderSummary(){
+        return this.page.locator(".confirm.btn.btn-lg.btn-primary").click();
+    }
+    
+}
+
+exports.HOMEPAGE_URL = "https://www.demoblaze.com/"
