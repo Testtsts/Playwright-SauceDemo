724642de9b4d6b856e25f0c1b9c4157226cec1f6
Testtsts <ihsannur0610@gmail.com>
2025-06-13 03:52:23 +0700

    swap color and size option sequence

--- tests/magneto/add-to-cart.spec.js
+++ tests/magneto/add-to-cart.spec.js
@@ -14,8 +14,8 @@ test('should succeed adding item to cart with selected size and color', async ({
   await homePage.searchForItem('Pierce');
   await expect(await catalogSearch.getNthProduct(0)).toBeVisible();
   await (await catalogSearch.getNthProduct(0)).click();
-  await(await (await (productDetail.getColorOption())).first()).click();
   await(await (await (productDetail.getSizeOption())).first()).click();
+  await(await (await (productDetail.getColorOption())).first()).click();
   await productDetail.fillQtyField(5);
   await productDetail.clickAddToCartButton();
   await expect(await globalElements.getCartCounter()).toHaveText('5');
