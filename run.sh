08dc1684e5879ba7d9a13c631f14a56d5fb17c8a
Testtsts <ihsannur0610@gmail.com>
2025-06-19 06:12:18 +0700

    fix var call

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 CONTAINER_NAME="playwright-test-container${2}"
 IMAGE_NAME="${1}"
-OUTPUT_FILE="playwright-resource-usage.txt${2}"
+OUTPUT_FILE="playwright-resource-usage${2}.txt"
 
 # Start the container
 # docker run --name "$CONTAINER_NAME" \
